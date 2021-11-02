64f86a7855d79e9cc766142f52d84afb242eb2e5
catherinecheng02 <z5310517@ad.unsw.edu.au>
2021-11-03 10:21:33 +1100

    Create multifile.c

---
+++ tut08/multifile.c
@@ -0,0 +1,23 @@
+// Multifile programs 
+// Tutorial Week 8 Question 1 - 3
+// Written by Catherine Cheng z5310517
+
+// Question 1
+// What is a Header file? What is its purpose?
+// - function declarations, #defines, struct declarations
+// - #include <stdio.h>
+// - #include "letters.h"
+// cannot access implementation, only has declarations
+
+// Question 2
+// What is an Implementation file? What is its purpose?
+// - implements the functions declared inside the header file
+
+
+// why do we need multi-file
+// - breaks up the project
+// - abstraction 
+// Question 3
+// How do we compile a multi-file project?
+
+

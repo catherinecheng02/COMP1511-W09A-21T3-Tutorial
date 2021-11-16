7747bb8fc1fa33dab2887ec720ea3dd23542b644
catherinecheng02 <z5310517@ad.unsw.edu.au>
2021-11-17 10:01:51 +1100

    Create stack.c

---
+++ tut10/stack.c
@@ -0,0 +1,19 @@
+// Abstract data types + stacks
+// Tutorial Week 10 Question 4 - 6
+// Written by Catherine Cheng z5310517
+
+// Question 4
+// What is an abstract data type? What are some examples?
+// - store data, but we don't know exactly how it's being stored
+// - linked lists 
+// - 
+
+ 
+// Question 5
+// Why do we use abstract data types? In what situations, do we use ADTs? 
+// - abstraction can be good because of security 
+
+// Question 6
+// What is a stack?
+
+// last in first out 
